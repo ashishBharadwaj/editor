864863309be31808e39b006eb3128e503604a07f
Ashish Bharadwaj <ashishbharadwaj@icloud.com>
2020-05-25 05:10:40 +0530

    Updates

---
+++ precache-manifest.3f4053e16d4bd6194c3c87a53fa2da48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2059355898bf4b7c4bef4a20d6fe6c78",
+    "url": "/editor/index.html"
+  },
+  {
+    "revision": "28ec7cbe73e657e091e3",
+    "url": "/editor/static/css/main.34b96188.chunk.css"
+  },
+  {
+    "revision": "19ffd37e0f2db54a0568",
+    "url": "/editor/static/js/2.4e44d93d.chunk.js"
+  },
+  {
+    "revision": "7a29fd67914f99c562dc3f65bbf1078b",
+    "url": "/editor/static/js/2.4e44d93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28ec7cbe73e657e091e3",
+    "url": "/editor/static/js/main.b02ff323.chunk.js"
+  },
+  {
+    "revision": "bc665728f69b50ad5885",
+    "url": "/editor/static/js/runtime-main.80305bf3.js"
+  }
+]);
